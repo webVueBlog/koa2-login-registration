17a208f1c53275fb9465648a9aaed4b95b8dcda3
webVueBlog <2979986590@qq.com>
2022-12-11 18:45:03 +0800

    add: 测试

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const Koa = require("koa")
+const path = require("path")
 const router = require("./router/router")
 const Cors = require("koa2-cors")
 const koaBody = require("koa-body")
+const source = require("koa-static")
 // Koa-router 请求方式： get 、 put 、 post 、 patch 、 delete 、 del 
 const app = new Koa()
 
 app.use(Cors())
-app.use(koaBody())
+app.use(koaBody({
+	multipart: true,
+	formidable: {
+		uploadDir: path.resolve(__dirname, 'temp'), // 文件存放地址
+		keepExtensions: true,
+		maxFieldsSize: 2 * 1024 * 1024
+	}
+}))
 app.use(router.routes())
-app.use(router.allowedMethods({}))
+app.use(router.allowedMethods())
+
+app.use(source(path.resolve(__dirname, "public/static")))
 
 console.log("项目启动http://127.0.0.1:3000")
 app.listen(3000)
